15bc8ae68b1ac623f29fe12a9682831a8b3584e0
porcha <porcha.por@mail.kmutt.ac.th>
2022-03-25 11:42:32 +0700

    new_feature Third committed

--- MyProject.java
+++ MyProject.java
@@ -6,6 +6,7 @@ public class MyProject {
 
     public static void main(String[] args) {
         System.out.println("Hello Worlds!");
+        System.out.println("Java Programming");
     }
     
 }
