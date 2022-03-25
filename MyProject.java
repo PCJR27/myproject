cdc2d03fa2a530f4f5c971e30dbe7a1affbe6c9f
porcha <porcha.por@mail.kmutt.ac.th>
2022-03-25 12:29:55 +0700

    5 committed

--- MyProject.java
+++ MyProject.java
@@ -1,13 +1,27 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package myproject;
 
-
+/**
+ *
+ * @author Porcha
+ */
 public class MyProject {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("Hello Worlds!");
         System.out.println("Java Programming");
         System.out.println("Hello World!");
+=======
+        // TODO code application logic here
+>>>>>>> parent of 60afb40 (second committed)
     }
     
 }
